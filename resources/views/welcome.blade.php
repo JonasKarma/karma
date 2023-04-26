8ec85fc90751aa780efc4605bfab5167f3b6fa21
JonasKarma <123567537+JonasKarma@users.noreply.github.com>
2023-04-26 18:02:05 +0100

    Include karma name in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
-                    <h1>Karma dev IT</h1>
+                    <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Karma dev IT</h1>
                 </div>
 
                 <div class="mt-16">
